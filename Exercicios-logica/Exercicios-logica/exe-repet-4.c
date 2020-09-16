1467cea419dc08e0234dc35b2b3e1a37ee78df98
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-09-15 22:00:11 -0300

    chore: add of C exercises

---
+++ Exercicios-logica/Exercicios-logica/exe-repet-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main1() {
+  int i;
+
+  for (i = 0; i < 10; i++)
+  {
+    printf("%i\n", i);
+  }
+   
+  return (0);
+}
---
+++ Exercicios-logica/aula5-c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+  int i;
+
+  for (i = 0; i <100; i++)
+  {
+    printf("3%i", i);
+  }
+  
+}
