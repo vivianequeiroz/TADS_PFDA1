6495ffe83a48215d5411cef0c42a53efae327ab6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-09-20 23:30:38 -0300

    chore: end of vector exercise from task 4

--- Exercicios-logica-IFSP/Vetores-Matrizes/tarefa4-exe3.c
+++ Exercicios-logica-IFSP/Vetores-Matrizes/tarefa4-exe3.c
@@ -3,25 +3,92 @@
 #include <locale.h>
 
 
-int main() {
+  int main() {
+  int numeros[4], i;
 
-    setlocale (LC_ALL, "Portuguese");
+  for (i = 0; i < 4; i++) {
+    printf("Digite : ");
+    scanf("%i", &numeros[i]);
+  }
 
-    int numbers[4], i;
+  for (i = 3; i > -1; i--) {
+    printf("\n%i", numeros[i]);
+  }
 
 
+  for (i = 0; i < 4; i++) { //numero par
+    if (i % 2)
+    {
+      printf("\n%i", numeros[i]);
+    }
+
+  }
+
+
+  for (i = 0; i < 4; i++) { // numero impar
+    if (!(i % 2))
+    {
+      printf("\n%i", numeros[i]);
+    }
+
+  // menor numero do vetor
+  int menorDado = numeros[0];
+  int indiceDado = 0;
+  for (i = 0; i < 4; i++) { 
+    if (numeros[i] < menorDado){
+      menorDado = numeros[i];
+      indiceDado = i;
+    }
+  }
+  printf("O menor dado no vetor eh %i e sua posicao eh %i", menorDado, indiceDado+1);
+    }
+
+  // maior numero do vetor
+  int maiorDado = numeros[0];
+  int indiceDado = 0;
+  for (i = 0; i < 4; i++) { 
+    if (numeros[i] > maiorDado){
+      maiorDado = numeros[i];
+      indiceDado = i;
+    }
+  }
+  printf("O menor dado no vetor eh %i e sua posicao eh %i", maiorDado, indiceDado+1);
+
+  int soma, mediaVetor;
     for (i = 0; i < 4; i++) { 
-    printf("Digite o número real: \n");
-    scanf("%i", &numbers[i]);
-    numbers[i] = numbers[i] * numbers[i];
+      soma = soma + numeros[i];
+
+  }
+  mediaVetor = soma / i;
+  printf("A media dos valores no vetor eh %i", mediaVetor);
+
+    int vetorRepetido[4] = { numeros[0], numeros[1], numeros[2], numeros[3] };
+
+    for(i = 0; i < 4; i++) {
+
+        for(int j = 0; j < 4; j++) {
+            if(numeros[i] == vetorRepetido[j]) {
+                printf("\n%i", numeros[i]);
+            }
+        }
+
+    }
+    int vetorRepetido[4] = { numeros[0], numeros[1], numeros[2], numeros[3] };
+
+    for(i = 0; i < 4; i++) {
+
+        for(int j = 0; j < 4; j++) {
+            if(!(numeros[i] == vetorRepetido[j])) {
+                printf("\n%i", numeros[i]);
+            }
+        }
+
     }
 
 
 
-    for (i = 3; i > -1; i--) { 
-    printf("%i \n", numbers[i]);
-    }
 }
+
 
 
 
